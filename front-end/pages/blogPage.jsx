45a002575e82e2fc26ec9f2e5a606af8382dd99d
2022LEAP2ZORIGOOBILGUUN <bilguunzorigooleap1@iMac2021-02s-iMac.local>
2022-10-05 12:19:38 +0800

    have to do sth

--- front-end/pages/blogPage.jsx
+++ front-end/pages/blogPage.jsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
 import { useNewsDatasContext } from "../context/newsDatasContext";
 import { Avatar } from "../components/index";
-import css from '../styles/BlogPage.module.css'
+import css from '../styles/BlogPage.module.css';
+import axios from "axios";
 const BlogPage = () => {
   const router = useRouter();
   const { newsDatas, setUserInterestedBlog } = useNewsDatasContext();
@@ -20,15 +21,12 @@ const BlogPage = () => {
             {newsDatas?.length<0? <p>loading</p>: (
               newsDatas?.map((blogData) => {
                 return (
-                  <div
-                    onClick={() => {
-                      setUserInterestedBlog(blogData);
-                      router.push("/blogDetail");
-                    }}
-                    className={css.Blog}
-                    key={blogData.id}
-                  >
+                  <div className={css.Blog} key={blogData.id}>
                     <img
+                      onClick={() => {
+                        setUserInterestedBlog(blogData);
+                        router.push("/blogDetail");
+                      }}
                       src={blogData.image}
                       style={{
                         objectFit: "cover",
@@ -47,12 +45,44 @@ const BlogPage = () => {
                       <p>{blogData.owner.firstName}</p>
                       <p>{blogData.publishDate}</p>
                     </div>
+                    <button
+                      onClick={async () => {
+                        // console.log
+                        await axios.delete(
+                          `http://localhost:8000/users/delete/${blogData.id}`,
+                          blogData,
+                          // {
+                          // id: "60d21b4667d0d8992e610c85",
+                          // image:
+                          // "https://ichef.bbci.co.uk/news/976/cpsprodpb/14E02/production/_110660558_hi059476725.jpg",
+                          // likes: 43,
+                          // tags: ["animal", "dog", "golden retriever"],
+                          // text: "adult Labrador retriever",
+                          // publishDate: "2020-05-24T14:53:17.598Z",
+                          // owner: {
+                          // id: "60d0fe4f5311236168a109ca",
+                          // title: "ms",
+                          // firstName: "Sara",
+                          // lastName: "Andersen",
+                          // picture:
+                          // "https://randomuser.me/api/portraits/women/58.jpg",
+                          // },
+                          // },
+                          {
+                            header: {
+                              "Content-type": "application/json; charset=UTF-8",
+                              "app-id": "6336455296396a087298080d",
+                            },
+                          }
+                        );
+                    }}>Hasah</button>
                   </div>
                 );
               })
             )}
           </div>
         </div>
+        {/* <button>hasah</button> */}
       </div>
     );
 }
