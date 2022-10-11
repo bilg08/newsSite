7a59a776b41de43b4fd8f4a96d2fe5f845249008
Bilguun <zorigoobilguun30@gmail.com>
2022-10-11 05:47:55 -0700

    chan

--- back-end/routers/userRouter.js
+++ back-end/routers/userRouter.js
@@ -2,5 +2,5 @@ import express from 'express';
 import {userController} from '../controllers/userController'
 const router = express.Router();
 
-router.post("/users/register", userController.createNewUser);
+router.post("/users/", userController.createNewUser);
 export default router; 
